4c612ce5ffc6b31e9b3fdb8e0a0be2dbcbc17b6d
greenaltambe <154662822+greenaltambe@users.noreply.github.com>
2025-07-30 21:48:11 +0530

    Fix problem routes, controllers

--- backend/controllers/problem.controller.js
+++ backend/controllers/problem.controller.js
@@ -1,36 +1,43 @@
+import asyncHandler from "express-async-handler";
+
 // @desc    Get all problems
 // @route   GET /api/problems
 // @access  Private
-const getProblems = (req, res) => {
+const getProblems = asyncHandler((req, res) => {
 	res.status(200).json({ message: "All problems" });
-};
+});
 
 // @desc    Get single problem
 // @route   GET /api/problems/:id
 // @access  Private
-const getProblem = (req, res) => {
+const getProblem = asyncHandler((req, res) => {
 	res.status(200).json({ message: `Get problem ${req.params.id}` });
-};
+});
 
 // @desc    Set problem
 // @route   POST /api/problems
 // @access  Private
-const setProblem = (req, res) => {
+const setProblem = asyncHandler((req, res) => {
+	if (!req.body.title) {
+		res.status(400);
+		throw new Error("Please add a title field");
+	}
+
 	res.status(200).json({ message: "Set problem" });
-};
+});
 
 // @desc    Update problem
 // @route   PUT /api/problems/:id
 // @access  Private
-const updateProblem = (req, res) => {
+const updateProblem = asyncHandler((req, res) => {
 	res.status(200).json({ message: `Update problem ${req.params.id}` });
-};
+});
 
 // @desc    Delete problem
 // @route   DELETE /api/problems/:id
 // @access  Private
-const deleteProblem = (req, res) => {
+const deleteProblem = asyncHandler((req, res) => {
 	res.status(200).json({ message: `Delete problem ${req.params.id}` });
-};
+});
 
 export { getProblems, getProblem, setProblem, updateProblem, deleteProblem };
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import errorHandler from "./middleware/error.middleware.js";
 import authRouter from "./routes/auth.routes.js";
 import problemsRouter from "./routes/problem.route.js";
 
@@ -8,8 +9,12 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 const app = express();
 
+app.use(express.urlencoded({ extended: false }));
+
 app.use("/api/auth", authRouter); // for login, register, logout
 app.use("/api/problems", problemsRouter); // for all problems
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
