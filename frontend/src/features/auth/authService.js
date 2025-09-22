dfe74068c75b735728a65a3078713ce32c445c4c
greenaltambe <154662822+greenaltambe@users.noreply.github.com>
2025-09-22 19:10:01 +0530

    Add deployment urls

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,38 +1,39 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/auth/";
+// const API_URL = "http://localhost:5000/api/auth/";
+const API_URL = "https://online-judge-1-mf6q.onrender.com/api/auth/";
 
 // Register user
 const register = async (userData) => {
-	const response = await axios.post(API_URL + "register", userData);
+  const response = await axios.post(API_URL + "register", userData);
 
-	if (response.data) {
-		console.log(response.data);
-		localStorage.setItem("user", JSON.stringify(response.data)); // store user in local storage
-	}
+  if (response.data) {
+    console.log(response.data);
+    localStorage.setItem("user", JSON.stringify(response.data)); // store user in local storage
+  }
 
-	return response.data;
+  return response.data;
 };
 
 const login = async (userData) => {
-	const response = await axios.post(API_URL + "login", userData);
+  const response = await axios.post(API_URL + "login", userData);
 
-	if (response.data) {
-		console.log(response.data);
-		localStorage.setItem("user", JSON.stringify(response.data)); // store user in local storage
-	}
+  if (response.data) {
+    console.log(response.data);
+    localStorage.setItem("user", JSON.stringify(response.data)); // store user in local storage
+  }
 
-	return response.data;
+  return response.data;
 };
 
 const logout = () => {
-	localStorage.removeItem("user"); // remove user from local storage
+  localStorage.removeItem("user"); // remove user from local storage
 };
 
 const authService = {
-	register,
-	logout,
-	login,
+  register,
+  logout,
+  login,
 };
 
 export default authService;
--- frontend/src/features/problem/problemService.js
+++ frontend/src/features/problem/problemService.js
@@ -1,98 +1,99 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/problems/";
+// const API_URL = "http://localhost:5000/api/problems/";
+const API_URL = "https://online-judge-1-mf6q.onrender.com/api/problems/";
 
 const getProblems = async (token) => {
-	const config = {
-		headers: {
-			Authorization: `Bearer ${token}`,
-		},
-	};
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
 
-	const response = await axios.get(API_URL, config);
+  const response = await axios.get(API_URL, config);
 
-	if (response.data) {
-		console.log(response.data);
-	}
-	return response.data;
+  if (response.data) {
+    console.log(response.data);
+  }
+  return response.data;
 };
 
 const getProblem = async (id, token) => {
-	const config = {
-		headers: {
-			Authorization: `Bearer ${token}`,
-		},
-	};
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
 
-	const response = await axios.get(API_URL + id, config);
-	console.log(response.data);
-	return response.data;
+  const response = await axios.get(API_URL + id, config);
+  console.log(response.data);
+  return response.data;
 };
 
 const createProblem = async (problemData, token) => {
-	const config = {
-		headers: {
-			Authorization: `Bearer ${token}`,
-		},
-	};
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
 
-	const response = await axios.post(API_URL, problemData, config);
-	return response.data;
+  const response = await axios.post(API_URL, problemData, config);
+  return response.data;
 };
 
 const deleteProblem = async (id, token) => {
-	const config = {
-		headers: {
-			Authorization: `Bearer ${token}`,
-		},
-	};
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
 
-	const response = await axios.delete(API_URL + id, config);
-	return response.data;
+  const response = await axios.delete(API_URL + id, config);
+  return response.data;
 };
 
 const runSolution = async (problemData, token) => {
-	const config = {
-		headers: {
-			Authorization: `Bearer ${token}`,
-		},
-	};
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
 
-	const response = await axios.post(API_URL + "run", problemData, config);
-	return response.data;
+  const response = await axios.post(API_URL + "run", problemData, config);
+  return response.data;
 };
 
 const submitSolution = async (problemData, token) => {
-	const config = {
-		headers: {
-			Authorization: `Bearer ${token}`,
-		},
-	};
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
 
-	const response = await axios.post(API_URL + "submit", problemData, config);
-	return response.data;
+  const response = await axios.post(API_URL + "submit", problemData, config);
+  return response.data;
 };
 
 const getSubmissions = async (id, token) => {
-	const config = {
-		headers: {
-			Authorization: `Bearer ${token}`,
-		},
-	};
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
 
-	const response = await axios.get(API_URL + id + "/submissions", config);
-	console.log(response);
-	return response.data;
+  const response = await axios.get(API_URL + id + "/submissions", config);
+  console.log(response);
+  return response.data;
 };
 
 const problemService = {
-	getProblems,
-	createProblem,
-	deleteProblem,
-	getProblem,
-	runSolution,
-	submitSolution,
-	getSubmissions,
+  getProblems,
+  createProblem,
+  deleteProblem,
+  getProblem,
+  runSolution,
+  submitSolution,
+  getSubmissions,
 };
 
 export default problemService;
