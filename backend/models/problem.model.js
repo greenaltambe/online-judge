96cc4269ec387c8df4d92fba0cfac458884b3506
greenaltambe <154662822+greenaltambe@users.noreply.github.com>
2026-07-06 20:30:17 +0530

    Add service directory, simplify controllers

--- backend/models/problem.model.js
+++ backend/models/problem.model.js
@@ -1,36 +1,40 @@
 import mongoose from "mongoose";
 
 const problemSchema = mongoose.Schema(
-	{
-		title: {
-			type: String,
-			required: true,
-		},
-		description: {
-			type: String,
-			required: true,
-		},
-		difficulty: {
-			type: String,
-			required: true,
-			enum: ["easy", "medium", "hard"],
-		},
-		testCases: [
-			{
-				input: {
-					type: String,
-					required: true,
-				},
-				expectedOutput: {
-					type: String,
-					required: true,
-				},
-			},
-		],
-	},
-	{
-		timestamps: true,
-	}
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    difficulty: {
+      type: String,
+      required: true,
+      enum: ["easy", "medium", "hard"],
+    },
+    topics: {
+      type: [String],
+      required: true,
+    },
+    testCases: [
+      {
+        input: {
+          type: String,
+          required: true,
+        },
+        expectedOutput: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  },
 );
 
 const Problem = mongoose.model("Problem", problemSchema);
